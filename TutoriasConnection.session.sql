df60572d0a2fe95a609bb00df252b78516e44797
spawmc <spawnmcsqrt@gmail.com>
2020-12-12 14:46:56 -0600

    Se modificaron registros de tutor y se modificaron valores de las tablas

--- TutoriasConnection.session.sql
+++ TutoriasConnection.session.sql
@@ -12,16 +12,11 @@ CREATE table alumno (
     nombre VARCHAR(20) NOT NULL,
     apellidoP VARCHAR(20) NOT NULL,
     apellidoM VARCHAR(20) NOT NULL,
-    email VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
     semestre INT(2) NOT NULL,
     diaIngreso INT(2) NOT NULL,
     mesIngreso INT(2) NOT NULL,
     yearIngreso INT(4) NOT NULL
-);
-CREATE table alumno_tutor (
-    matricula VARCHAR(10) NOT NULL,
-    numpersonalTutor INT(2) NOT NULL,
-    FOREIGN KEY (matricula) REFERENCES alumno(matricula)
 );
 CREATE table tutor (
     numpersonal INT(9) PRIMARY KEY NOT NULL,
@@ -29,6 +24,11 @@ CREATE table tutor (
     apellidoP VARCHAR(20) NOT NULL,
     apellidoM VARCHAR(20) NOT NULL,
     email VARCHAR(20) NOT NULL
+);
+CREATE table alumno_tutor (
+    matricula VARCHAR(10) NOT NULL,
+    numpersonalTutor INT(2) NOT NULL,
+    FOREIGN KEY (matricula) REFERENCES alumno(matricula)
 );
 CREATE table tutoria (
     noTutoria INT(2) NOT NULL,
@@ -65,14 +65,16 @@ insert into alumno ( matricula, nombre, apellidoP, apellidoM, email, semestre, d
 insert into alumno ( matricula, nombre, apellidoP, apellidoM, email, semestre, diaIngreso, mesIngreso, yearIngreso ) values ( 'S19015920', 'Sofia', 'Lopez', 'Gonzalez', 'S19015920@estudiantes.uv.mx', 7, 06, 07, 2017 );
 insert into alumno ( matricula, nombre, apellidoP, apellidoM, email, semestre, diaIngreso, mesIngreso, yearIngreso ) values ( 'S19014781', 'Karla', 'Landa', 'Ruiz', 'S19014781@estudiantes.uv.mx', 7, 10, 12, 2017 );
     /* Tutor */
-insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190152837, 'Alizia', 'Lozano', 'Marques', 'ecovi91@outlook.com' );
-insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190152983, 'Nestor', 'Pujol', 'Beltran', 'ecovi91@outlook.com' );
-insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190159832, 'Enric', 'Jover', 'Infante', 'ecovi91@outlook.com' );
-insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190152782, 'Irene', 'Canales', 'Pulido', 'ecovi91@outlook.com' );
-insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190152394, 'Virgilio', 'Lema', 'Mendez', 'ecovi91@outlook.com' );
-insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190153981, 'Graciela', 'Aguado', 'Hernandez', 'ecovi91@outlook.com' );
+insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190152837, 'Alizia', 'Lozano', 'Marques', '190152837@tutores.uv.mx' );
+insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190152983, 'Nestor', 'Pujol', 'Beltran', '190152983@tutores.uv.mx' );
+insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190159832, 'Enric', 'Jover', 'Infante', '190159832@tutores.uv.mx' );
+insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190152782, 'Irene', 'Canales', 'Pulido', '190152782@tutores.uv.mx' );
+insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190152394, 'Virgilio', 'Lema', 'Mendez', '190152394@tutores.uv.mx' );
+insert into tutor (numpersonal, nombre, apellidoP, apellidoM, email) values ( 190153981, 'Graciela', 'Aguado', 'Hernandez', '190153981@tutores.uv.mx' );
     /*alumno_turo TODO: AGREGAR MAS REGISTROS PARA ALUMNO_TUTOR*/ 
-insert into alumno_tutor (matricula, numpersonal) values('S19012345', )
+
+insert into alumno_tutor (matricula, numpersonal) values('S19012345', 190152837)
+
 insert into tutoria (
         NoTutoria,
         PeriodoInicial,

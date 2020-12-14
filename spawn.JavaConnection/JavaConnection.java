c93cae8e7d98461b8db203878a6fac2ff6a3be6a
spawmc <spawnmcsqrt@gmail.com>
2020-12-14 11:59:59 -0600

    Se agrego el constructor y la funcion para generar consultas

--- spawn.JavaConnection/JavaConnection.java
+++ spawn.JavaConnection/JavaConnection.java
@@ -2,13 +2,52 @@ package spawn.JavaConnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.JOptionPane;
+
 public class JavaConnection {
+    /**
+     * Configuración basica para la conección
+     */
     private String user = "javaConnection";
     private String pwd = "javaConnection";
     private static String db = "Tutoria";
     private String url = "jdbc:mariadb://localhost/" + db;
     private Connection conn = null;
+
+    /**
+     * Constructor para iniciar la conección a la base de datos
+     */
+    public JavaConnection() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver"); // Driver para mariaDB
+            conn = (Connection) DriverManager.getConnection(url, user, pwd);
+            if (conn != null) {
+                JOptionPane.showMessageDialog(null, "¡Conexión con exito!", "", JOptionPane.INFORMATION_MESSAGE);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Problemas al conectar a la base de datos :(", "ERROR",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    @Override
+    public ResultSet getQuery(String query){
+        Statement state = null; // Para la creacion de la consulta
+        ResultSet resultado = null;
+        try {
+            state = (Statement) conn.createStatement();
+            resultado = state.executeQuery(query);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return resultado;
+    }
+
+
 }
